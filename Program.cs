7baca1244a8ea1516f22699298d4dbf05b2b69fb
Admin <Admin@WIN-0QFN66U68EP>
2024-10-03 18:21:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class CalculateExpression
+{
+    static void Main(string[] args)
+    {
+        // Запрашиваем у пользователя действительное число x
+        Console.Write("Введите действительное число x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        // Вычисляем выражение
+        double x2 = x * x;
+        double x4 = x2 * x2;
+        double result = 7 + x * (2 * x2 - 5 * x + x4 * 3);
+
+        // Выводим результат на консоль
+        Console.WriteLine($"Результат: {result}");
+    }
+}
